11190673da3f2bb3b80eba0457cb231b4cde1189
ZachO <zachjoliver@gmail.com>
2019-05-24 16:51:13 -0700

    End of phase 5

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -1,14 +1,17 @@
 require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
+require 'rack'
+require 'byebug'
 require_relative './session'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
-    @req, @res = req, res
+  def initialize(req, res, params = {})
+    @req, @res, @params = req, res, params
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
@@ -21,6 +24,7 @@ class ControllerBase
     raise 'Double render error' if already_built_response?
     @res.set_header('Location', url)
     @res.status = 302   #@res[]=
+    session.store_session(@res)
     @already_built_response = true
   end
 
@@ -29,22 +33,32 @@ class ControllerBase
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
     raise 'Double render error' if already_built_response?
-    @already_built_response = true
     @res['Content-Type'] = content_type
     @res.write(content)
+    session.store_session(@res)
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    #file = File.dirname("views/#{self.class.to_s.underscore}/#{template_name}.html.erb") #.join
+    file = File.join(File.dirname(__FILE__),"..","views", self.class.to_s.underscore, "#{template_name}.html.erb")
+    content = File.read(file)
+    template = ERB.new(content).result(binding)
+    render_content(template, 'text/html')
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+      # self.send(name)
+      self.try(name)
+      render(name) unless already_built_response?
   end
 end
 
--- skeleton/lib/session.rb
+++ skeleton/lib/session.rb
@@ -1,19 +1,30 @@
 require 'json'
+require 'byebug'
 
 class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    @req = req
+    sesh_cookie_str = @req.cookies['_rails_lite_app']
+    if sesh_cookie_str
+      @sesh_cookie = JSON.parse(sesh_cookie_str)
+    else
+      @sesh_cookie = {}
+    end
   end
 
   def [](key)
+    @sesh_cookie[key]
   end
 
   def []=(key, val)
+    @sesh_cookie[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.set_cookie("_rails_lite_app", { path: "/" , value: @sesh_cookie.to_json })
   end
 end
